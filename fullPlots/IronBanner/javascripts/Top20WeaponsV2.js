0614e901af505d096e0ba59aa724037ed3827925
Giovanni A Briggs <gbriggs2012@gmail.com>
2015-07-19 10:11:52 -0700

    Trying round 3

--- fullPlots/IronBanner/javascripts/Top20WeaponsV2.js
+++ fullPlots/IronBanner/javascripts/Top20WeaponsV2.js
@@ -13,8 +13,8 @@
 
 
                 //var width = $('#Top20WeaponsV2').width() - 60 - 60;
-                //width = "100%"
-                //chart.width(width);
+                var width = "100%"
+                chart.width(width);
                 var height = 450 - 30 - 20;
                 chart.height(height);
 
