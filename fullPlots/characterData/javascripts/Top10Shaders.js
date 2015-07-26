d2bd6c68b9a449cdfd2b2ec617c5d038a9907227
Giovanni A Briggs <gbriggs2012@gmail.com>
2015-07-26 12:40:08 -0700

    Fixed mobile bug on character plots

--- fullPlots/characterData/javascripts/Top10Shaders.js
+++ fullPlots/characterData/javascripts/Top10Shaders.js
@@ -13,7 +13,7 @@
 
 
                 var width = $('#Top10Shaders').width() - 60 - 60;
-                chart.width(width);
+                //chart.width(width);
                 var height = 450 - 30 - 20;
                 chart.height(height);
 
@@ -36,7 +36,7 @@
             d3.select('#Top10Shaders svg')
             .datum(datum)
             .transition().duration(500)
-            .attr('width', $('#Top10Shaders').width())
+            //.attr('width', $('#Top10Shaders').width())
             .attr('height', 450)
             .call(chart);
 
