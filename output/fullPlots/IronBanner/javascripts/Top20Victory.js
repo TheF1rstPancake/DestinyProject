0b995db9a94994b363228d7d938b5c123b5ae2af
Giovanni A Briggs <gbriggs2012@gmail.com>
2015-07-30 23:51:03 -0700

    Trying to use pelican to create blog site

---
+++ output/fullPlots/IronBanner/javascripts/Top20Victory.js
@@ -0,0 +1,46 @@
+
+
+                data_Top20Victory=[{"values": [{"y": 0.51214644553024569, "x": "Thorn"}, {"y": 0.50639966969446737, "x": "The Last Word"}, {"y": 0.4919673426389255, "x": "Red Death"}, {"y": 0.50538389513108617, "x": "Matador 64"}, {"y": 0.51485963612414587, "x": "Party Crasher +1"}, {"y": 0.52689221427022337, "x": "Felwinter's Lie"}, {"y": 0.58957789082909384, "x": "BTRD-345"}, {"y": 0.49077944430784359, "x": "Vex Mythoclast"}, {"y": 0.50663825602360268, "x": "Found Verdict"}, {"y": 0.59076795694473194, "x": "Jolder's Hammer"}, {"y": 0.50428163653663183, "x": "MIDA Multi-Tool"}, {"y": 0.50256410256410255, "x": "Judgment VI"}, {"y": 0.50022311468094594, "x": "Bad Juju"}, {"y": 0.49176867876741237, "x": "Ice Breaker"}, {"y": 0.47614840989399299, "x": "SUROS Regime"}, {"y": 0.52517223105458399, "x": "The Messenger (Adept)"}, {"y": 0.6195286195286196, "x": "Radegast's Fury"}, {"y": 0.49798657718120809, "x": "Praedyth's Revenge"}, {"y": 0.49419504643962853, "x": "Her Courtesy"}, {"y": 0.4994959677419355, "x": "Invective"}], "key": "Weapons", "yAxis": "1"}];
+
+
+                nv.addGraph(function() {
+            var chart = nv.models.discreteBarChart();
+
+            chart.margin({top: 30, right: 60, bottom: 20, left: 60});
+
+            var datum = data_Top20Victory;
+
+
+
+                var width = $('#Top20Victory').width() - 60 - 60;
+                chart.width(width);
+                var height = 450 - 30 - 20;
+                chart.height(height);
+
+
+
+
+                    chart.xAxis
+                .rotateLabels(-25)                .axisLabel('Weapon Name');
+            chart.yAxis
+                .axisLabel('Victory Rate')                .tickFormat(d3.format(',.2%'));
+
+    
+    
+            nv.utils.windowResize(function(){ chart.update(); });
+
+        
+
+
+
+            d3.select('#Top20Victory svg')
+            .datum(datum)
+            .transition().duration(500)
+            .attr('width', $('#Top20Victory').width())
+            .attr('height', 450)
+            .call(chart);
+
+    
+        });
+
+
---
+++ output/fullPlots/IronBanner/javascripts/Top20WeaponsCR.js
@@ -0,0 +1,46 @@
+
+
+                data_Top20WeaponsCR=[{"values": [{"y": 119.5663612737157, "x": "Radegast's Fury"}, {"y": 117.42378414279538, "x": "Jolder's Hammer"}, {"y": 114.93559539042451, "x": "The Messenger (Adept)"}, {"y": 113.93470797459406, "x": "BTRD-345"}, {"y": 111.0713835823443, "x": "Felwinter's Lie"}, {"y": 107.02076547589017, "x": "Judgment VI"}, {"y": 106.49767979323452, "x": "Matador 64"}, {"y": 105.9073672589917, "x": "Party Crasher +1"}, {"y": 105.37986870446747, "x": "The Last Word"}, {"y": 104.80136852839968, "x": "Thorn"}, {"y": 103.54092299966402, "x": "Praedyth's Revenge"}, {"y": 102.61274085355583, "x": "Found Verdict"}, {"y": 101.52935417673817, "x": "Bad Juju"}, {"y": 100.64518285688281, "x": "Red Death"}, {"y": 99.805674308294286, "x": "Her Courtesy"}, {"y": 98.954198205057452, "x": "Invective"}, {"y": 98.697023588961969, "x": "MIDA Multi-Tool"}, {"y": 97.863588122945785, "x": "Vex Mythoclast"}, {"y": 92.307581654389068, "x": "SUROS Regime"}, {"y": 91.753760408100433, "x": "Ice Breaker"}], "key": "Weapons", "yAxis": "1"}];
+
+
+                nv.addGraph(function() {
+            var chart = nv.models.discreteBarChart();
+
+            chart.margin({top: 30, right: 60, bottom: 20, left: 60});
+
+            var datum = data_Top20WeaponsCR;
+
+
+
+                var width = $('#Top20WeaponsCR').width() - 60 - 60;
+                chart.width(width);
+                var height = 450 - 30 - 20;
+                chart.height(height);
+
+
+
+
+                    chart.xAxis
+                .rotateLabels(-25)                .axisLabel('Weapon Name');
+            chart.yAxis
+                .axisLabel('Frequency')                .tickFormat(d3.format(',.2f'));
+
+    
+    
+            nv.utils.windowResize(function(){ chart.update(); });
+
+        
+
+
+
+            d3.select('#Top20WeaponsCR svg')
+            .datum(datum)
+            .transition().duration(500)
+            .attr('width', $('#Top20WeaponsCR').width())
+            .attr('height', 450)
+            .call(chart);
+
+    
+        });
+
+
